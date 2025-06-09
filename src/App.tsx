6bfb408f3c7ef322bc8958b50a8e2cc5bda96087
Bosshentai <baptistamhernani@gmail.com>
2025-06-09 22:25:26 -0100

    feat: finish add mui and i can use tailwind v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,9 @@
-import reactLogo from './assets/react.svg';
-import { Counter } from './features/counter/Counter';
+import Button from '@mui/material/Button';
 
 // import './App.css';
 
 function App() {
-  return (
-    <div className="text-center">
-      More actions
-      <header className="min-h-screen flex flex-col items-center justify-center text-2xl">
-        <img src={reactLogo} className="h-[40vmin] pointer-none" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="purple-800"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="purple-800"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="purple-800"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="purple-800"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+  return <Button variant="contained">Hello Word</Button>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { store } from './app/store.ts';
+import { StyledEngineProvider } from '@mui/material';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </StyledEngineProvider>
   </StrictMode>,
 );
